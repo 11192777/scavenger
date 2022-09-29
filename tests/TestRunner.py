dc5caa2d1adda8fdb648de7f4a5d9f5140f05d72
yali.liu <yali.liu@huilianyi.com>
2022-09-29 19:22:30 +0800

    fix:HtmlTestRunner env .

--- tests/TestRunner.py
+++ tests/TestRunner.py
@@ -1,12 +1,12 @@
 import unittest
 
 import FormTests
-from HtmlTestRunner import HTMLTestRunner
+import HtmlTestRunner
 
 suite = unittest.TestSuite()
 loader = unittest.TestLoader()
 suite.addTest(loader.loadTestsFromTestCase(FormTests.FormFieldTests))
 
 if __name__ == '__main__':
-    runner = HTMLTestRunner(output='./report', report_title=u'测试报告')
+    runner = HtmlTestRunner.HTMLTestRunner(output='./report', report_title=u'测试报告')
     runner.run(suite)
