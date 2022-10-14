9d742272be05de0af21bf4d071a96dc47fedd2b7
Qingyu.Meng <11192777@qq.com>
2022-10-14 18:02:23 +0800

    feat: 🎸 整合测试用例

--- tests/TestRunner.py
+++ tests/TestRunner.py
@@ -5,7 +5,9 @@ import HtmlTestRunner
 
 suite = unittest.TestSuite()
 loader = unittest.TestLoader()
-suite.addTest(loader.loadTestsFromTestCase(FormTests.FormFieldTests))
+suite.addTests(loader.loadTestsFromTestCase(FormTests.ArchiveTypeTests))
+suite.addTests(loader.loadTestsFromTestCase(FormTests.DocumentTypeTests))
+suite.addTests(loader.loadTestsFromTestCase(FormTests.FormFieldTests))
 
 if __name__ == '__main__':
     runner = HtmlTestRunner.HTMLTestRunner(output='./report', report_title=u'测试报告')
