0ae1eea44c5aef58145005918c8d4c481595b490
Qingyu.Meng <11192777@qq.com>
2022-10-18 09:18:43 +0800

    feat: 🎸 debug with gitscript

--- browser/HeliosGit.py
+++ browser/HeliosGit.py
@@ -5,43 +5,36 @@ import requests
 from requests.cookies import RequestsCookieJar
 
 
-class HeliosGit:
-
-    def __init__(self):
-        self.cookies = RequestsCookieJar()
-        self.cookies.set("lang", "lang")
-        self.cookies.set("hy_data_2020_id", "18339f5d0343e-075464d74af03f-1a525635-3686400-18339f5d035155")
-        self.cookies.set("hy_data_2020_js_sdk", "%7B%22distinct_id%22%3A%2218339f5d0343e-075464d74af03f-1a525635-3686400-18339f5d0351556%22%2C%22site_id%22%3A357%2C%22user_company%22%3A266%2C%22props%22%3A%7B%7D%2C%22device_id%22%3A%2218339f5d0343e-075464d74af03f-1a525635-3686400-18339f5d0351556%22%7D")
-        self.cookies.set("Hm_lvt_343b9b8fc424627f20938ba2a6d10b47", "Hm_lvt_343b9b8fc424627f20938ba2a6d10b47")
-        self.cookies.set("Qs_lvt_337518", "1660876247%2C1662743027%2C1663124754%2C1665546985%2C1665645018")
-        self.cookies.set("Qs_pv_337518", "2362588262057168000%2C3127977143522190000%2C338456138017873860%2C1394974043157306600%2C3274612798727682600")
-        self.cookies.set("helios_git_lucky", "03e636fb2557330a")
-        self.cookies.set("_csrf", "CFYLvv64iMx9nD2iP_n2Lk9kNlU6MTY2NTk4OTU2NjI3NTMzOTk3MA%3D%3D")
-        self.cookies.set("redirect_to", "%252F")
-
-
-
-    def response_text(self, method, url, **kwargs):
-        """请求函数"""
-        response = requests.request(method, url, cookies=self.cookies, **kwargs)  # 发送请求带入cookies
-        result = response.text
-        self.cookies.update(response.cookies)  # 更新cookies
-        return result
-
-    def cookires(self):
-        return self.cookies
-
-
 if __name__ == '__main__':
-    git = HeliosGit()
-    login_url = "https://code.huilianyi.com/user/login" + urlencode({"_csrf": "spzech8jcVLTarWDez2sUp1YOTA6MTY2NTk4ODEzMjA5NzM0NDMzOA==", "user_name": "qingyu.meng@huilianyi.com", "password": "Qingyu981117", "remember": "on"})
-    git.response_text("POST", login_url)
+    csrf = "6K3CaQO5936TJgXTipfy36khn106MTY2NTk5NDg0NDAwMDc5MDI0Mg=="
+    cookies = RequestsCookieJar()
+    cookies.set("lang", "zh-CN")
+    cookies.set("Hm_lvt_3c193caea742eb1d364949dfa96cea20", "1661486186,1661525852,1661737666,1663341034")
+    cookies.set("Hm_lvt_343b9b8fc424627f20938ba2a6d10b47", "1661486186,1661525852,1661737666,1663341035")
+    cookies.set("Qs_lvt_337518", "1661418127%2C1661477446%2C1661525853%2C1661737666%2C166334103")
+    cookies.set("Qs_pv_337518", "2299340530736051200%2C865690689348363800%2C442714974431105600%2C978892390697045200%2C169423104547499300")
+    cookies.set("helios_git_lucky", "3c9a8d466d502197")
+    cookies.set("_csrf", "6K3CaQO5936TJgXTipfy36khn106MTY2NTk5NDg0NDAwMDc5MDI0Mg%3D%3D")
+    cookies.set("redirect_to", "%252F")
 
-    code_huilianyi = "https://code.huilianyi.com"
-    print(git.response_text("GET", code_huilianyi))
+    data = {
+        "_csrf": csrf,
+        "user_name": "qingyu.meng@huilianyi.com",
+        "password": "Qingyu981117",
+        "remember": "on"
+    }
+    response1 = requests.get("https://code.huilianyi.com/user/login", data=data, headers={"Content-Type": "application/x-www-form-urlencoded"})
+    cookies.update(response1.cookies)
 
-    print(git.cookies)
+    response2 = requests.get("https://code.huilianyi.com")
+    cookies.update(response2.cookies)
 
-    print(unquote(git.cookies.get("_csrf")))
-    merge_url = "https://code.huilianyi.com/yali.liu/scavenger/compare/master...yali.liu:dev" + urlencode({"_csrf": unquote(git.cookies.get("_csrf")), "title": "66666"})
-    print(git.response_text("POST", merge_url))
+    csrf = "QZQw5K_vNhGyo38USdlR0SB38r06MTY2NjAwMDMxNTgzNjIzMTgzOQ=="
+
+    data = {
+        "_csrf": csrf,
+        "title": "66666"
+    }
+    print(cookies)
+    response3 = requests.post("https://code.huilianyi.com/yali.liu/scavenger/compare/master...yali.liu:dev", data=data, headers={"Content-Type": "application/x-www-form-urlencoded"})
+    print(response3.text)
