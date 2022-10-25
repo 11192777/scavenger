b75a1bd7b34db88c4c734e400069cc496c9aaf9e
Qingyu.Meng <11192777@qq.com>
2022-10-25 18:37:39 +0800

    feat: 🎸 增加git提交日志

--- apitests/ApiTests.py
+++ apitests/ApiTests.py
@@ -8,7 +8,8 @@ ENV = LoginServer.local_private_dev_11192777()
 
 class ApiTests(unittest.TestCase):
 
-    def test_addArchiveType(self):
+    def setUp(self):
         self.formServer = FormServer(ENV)
 
-    
+    def test_新增档案类型(self):
+        print(self.formServer.random_archive_form(business_type="GC").response)
--- browser/HeliosGit.py
+++ browser/HeliosGit.py
@@ -38,7 +38,8 @@ class HeliosGit:
 
     def login(self):
         url = "https://code.huilianyi.com/user/login"
-        response = requests.post(url=url, data={"user_name": "qingyu.meng@huilianyi.com", "password": "Qingyu981117"}, cookies=self.cookies, headers={"Content-Type": "application/x-www-form-urlencoded"})
+        response = requests.post(url=url, data={"user_name": "qingyu.meng@huilianyi.com", "password": "Qingyu981117"},
+                                 cookies=self.cookies, headers={"Content-Type": "application/x-www-form-urlencoded"})
         self.cookies.update(response.cookies)
         url = "https://code.huilianyi.com/"
         response = requests.get(url=url, cookies=self.cookies)
@@ -46,6 +47,7 @@ class HeliosGit:
 
     def compare(self):
         url = "https://code.huilianyi.com/{}/{}/compare/{}...{}:{}".format(self.path, self.info["project_name"], self.info["branch_name"], self.info["user_name"], self.info["remote_branch_name"])
+        print("===> Compare url is:{}".format(url))
         response = requests.get(url=url, cookies=self.cookies)
         html = etree.HTML(response.text)
         commits = html.xpath("/html/body/div[@class='full height']/div[@class='repository compare pull diff']/div[@class='ui container']/div[@class='sixteen wide column page grid']/div[@class='ui unstackable attached table segment']/table[@id='commits-table']/tbody/tr")
@@ -60,7 +62,8 @@ class HeliosGit:
     def merge(self, title):
         url = "https://code.huilianyi.com/{}/{}/compare/{}...{}:{}".format(self.path, self.info["project_name"], self.info["branch_name"], self.info["user_name"], self.info["remote_branch_name"])
         print("===> Merge url is:{} Title is:{}".format(url, title))
-        response = requests.post(url=url, data={"title": title, "_csrf": unquote(self.cookies.get("_csrf"))}, cookies=self.cookies, headers={"Content-Type": "application/x-www-form-urlencoded"})
+        response = requests.post(url=url, data={"title": title, "_csrf": unquote(self.cookies.get("_csrf"))}, cookies=self.cookies, headers={"Content-Type": "application/x-www-form-urlencoded"}, allow_redirects=False)
+        print("===> Pull address url is: https://code.huilianyi.com{}".format(response.headers["location"]))
 
     def run(self):
         self.access()
@@ -96,9 +99,8 @@ class HeliosGit:
             items = line.split()
             if items[0] == remote_name:
                 target = re.findall(r'https:\/\/code.huilianyi.com\/(.+?)\.git', items[1])[0]
-                self.info["project_name"] = target.split("/")[0]
-                self.info["user_name"] = target.split("/")[1]
-
+                self.info["user_name"] = target.split("/")[0]
+                self.info["project_name"] = target.split("/")[1]
                 break
             line = remove_v.readline()
         self.info["commit_username"] = os.popen("git config user.name").read().split()[0]
