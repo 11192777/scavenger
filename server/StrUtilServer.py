962ea5859445db3ae61258384cf2b569dd193111
Qingyu.Meng <11192777@qq.com>
2023-02-23 17:01:50 +0800

    feat: 🎸 增加飞书消息接受API

--- server/StrUtilServer.py
+++ server/StrUtilServer.py
@@ -1,8 +1,13 @@
+import json
+
+import requests
+
 
 class StrUtilServer:
     apiSelector = [
         "Java str to py format",
-        "Java class to JSON"
+        "Java class to JSON",
+        "发送到飞书"
     ]
 
     def __init__(self, type, content):
@@ -14,6 +19,8 @@ class StrUtilServer:
             return self.javaStrToPyFormat()
         elif self.type == self.apiSelector[1]:
             return self.javaClassToJSON()
+        elif self.type == self.apiSelector[2]:
+            return self.sendToFeishu()
 
     def javaStrToPyFormat(self):
         data = str(self.content)
@@ -24,4 +31,23 @@ class StrUtilServer:
     def javaClassToJSON(self):
         data = str(self.content)
 
-
+    def sendToFeishu(self):
+        data = {
+            "msg_type": "post",
+            "content": {
+                "post": {
+                    "zh_cn": {
+                        "title": "消息接受",
+                        "content": [
+                            [
+                                {
+                                    "tag": "text",
+                                    "text": self.content
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        }
+        requests.post("https://open.feishu.cn/open-apis/bot/v2/hook/4609eee3-9e69-4140-849a-31a4a6e7d550", data=json.dumps(data))
