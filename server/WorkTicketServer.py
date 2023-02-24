2d4f577088f9757215b84128d90459df0eea15a1
Qingyu.Meng <11192777@qq.com>
2023-02-24 11:00:14 +0800

    feat: 🎸 工单生成增加SELECE SQL

--- server/WorkTicketServer.py
+++ server/WorkTicketServer.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import re
 import time
 from itertools import groupby
 
@@ -14,27 +15,36 @@ class WorkTicketServer:
         self.content = json.loads(content)
         self.operator = operator
         self.lastModifyDate = time.strftime("-%Y%m%d", time.localtime())
+        self.formCodes = ", ".join(["'{}'".format(str(item).replace(" ", "")) for item in re.split(r'[,，|?？]', self.content["formCodes"])])
+        self.fieldCodes = ", ".join(["'{}'".format(str(item).replace(" ", "")) for item in re.split(r'[,，|?？]', self.content["fieldCodes"])])
+        self.tenantId = self.content["tenantId"]
 
     def execute(self):
+        target = {}
         if self.operator == "文本扩容":
-            return self.generate({
-                "WORDS_LIMIT": self.content["wordsLimit"],
-                "MAX_WORDS_LIMIT": self.content["wordsMaxLimit"]
-            })
+            if self.content["wordsLimit"] != '':
+                target["WORDS_LIMIT"] = self.content["wordsLimit"]
+            if self.content["wordsMaxLimit"] != '':
+                target["MAX_WORDS_LIMIT"] = self.content["wordsMaxLimit"]
         elif self.operator == "文本数组扩容":
-            return self.generate({
-                "ITEM_WORDS_LIMIT": self.content["wordsLimit"],
-                "MAX_ITEM_WORDS_LIMIT": self.content["wordsMaxLimit"],
-                "ITEMS_LIMIT": self.content["itemLimit"],
-                "MAX_ITEMS_LIMIT": self.content["itemMaxLimit"],
-            })
+            if self.content["wordsLimit"] != '':
+                target["ITEM_WORDS_LIMIT"] = self.content["wordsLimit"]
+            if self.content["wordsMaxLimit"] != '':
+                target["MAX_ITEM_WORDS_LIMIT"] = self.content["wordsMaxLimit"]
+            if self.content["itemLimit"] != '':
+                target["ITEMS_LIMIT"] = self.content["itemLimit"]
+            if self.content["itemMaxLimit"] != '':
+                target["MAX_ITEMS_LIMIT"] = self.content["itemMaxLimit"]
+        return self.generate(target)
 
     def generate(self, widgetTypeProperty):
-        result = str({key: value for key, value in widgetTypeProperty.items() if value is not None}).replace(" ", "").replace("'", '"')
-        targetSql = FileUtils.loadStr("WorkTicketFormat").format(self.lastModifyDate, result, self.content["tenantId"],
-                                                            ",".join(["'{}'".format(item) for item in
-                                                                      self.content["formCodes"].split(',')]),
-                                                            ",".join(["'{}'".format(item) for item in
-                                                                      self.content["fieldCodes"].split(',')]))
-        FileUtils.appendLog("WorkTicketLog", time.strftime("==========> %Y-%m-%d %H:%M:%S\n", time.localtime()) + targetSql + "\n<==========\n\n")
-        return targetSql
+        properties = str({key: value for key, value in widgetTypeProperty.items() if value is not None}).replace(" ", "").replace("'", '"')
+        selectSql = FileUtils.loadStr("WorkTicketFormat", 13, 21).format(self.tenantId, self.formCodes, self.fieldCodes)
+        updateSql = FileUtils.loadStr("WorkTicketFormat", 1, 10).format(self.lastModifyDate, properties, self.tenantId, self.formCodes, self.fieldCodes)
+        FileUtils.appendLog("WorkTicketLog", time.strftime("==========> %Y-%m-%d %H:%M:%S\n", time.localtime()) + updateSql + "\n<==========\n\n")
+        return "{}\n\n\n\n\n{}".format(selectSql, updateSql)
+
+
+if __name__ == '__main__':
+    wo = WorkTicketServer('''{"wordsLimit":"","wordsMaxLimit":"","itemLimit":"800","itemMaxLimit":"800","tenantId":"1178518358079496194","formCodes":"SA_INVOICE,SA_INVOICE,SA_INVOICE ","fieldCodes":"L_serial_no"}''', "文本数组扩容")
+    print(wo.execute())
