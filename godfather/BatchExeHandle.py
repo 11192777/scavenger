969e1ad7569de167d247c1763db0deaa64b01c74
yubaby <11192777@qq.com>
2024-02-23 23:57:00 +0800

    test

--- godfather/BatchExeHandle.py
+++ godfather/BatchExeHandle.py
@@ -6,11 +6,15 @@ import logging
 def aliceRebuild():
     try:
         homePath = os.environ.get("ALICE_HOME")
-        pid = OSBashUtils.getPid("7077")
-        OSBashUtils.killProcess(pid)
+        pid = yield from OSBashUtils.getPid("7077")
+        yield from OSBashUtils.killProcess(pid)
         yield from OSBashUtils.gitPull(homePath)
         yield from OSBashUtils.mvnCleanInstall(f=homePath + "/alice-start/pom.xml")
         yield from OSBashUtils.springRun(jar=homePath + "/alice-start/target/alice.jar", active="ganquan-release")
     except Exception as e:
         logging.error(f"Caught an exception: {e}")
         yield f"data: ======> CMD FAILED: {e.args}\n\n"
+
+
+if __name__ == '__main__':
+    aliceRebuild()
