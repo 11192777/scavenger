031622828af87e43c95aa094da93771d91a65d06
yubaby <11192777@qq.com>
2024-02-23 23:22:30 +0800

    fix:alice build

--- godfather/BatchExeHandle.py
+++ godfather/BatchExeHandle.py
@@ -7,7 +7,7 @@ def aliceRebuild():
     try:
         homePath = os.environ.get("ALICE_HOME")
         yield from OSBashUtils.gitPull(homePath)
-        yield from OSBashUtils.mvnCleanInstall(f=homePath + "/pom.xml")
+        yield from OSBashUtils.mvnCleanInstall(f=homePath + "/alice-start/pom.xml")
         yield from OSBashUtils.springRun(jar=homePath + "/alice-start/target/alice.jar")
     except Exception as e:
         logging.error(f"Caught an exception: {e}")
